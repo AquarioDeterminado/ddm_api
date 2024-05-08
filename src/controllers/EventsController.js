6d819e05fda18f516be986be3249fdaedbf28080
[Aquario Determinado] <aquario.determinado@gmail.com>
2024-05-08 18:39:19 +0100

    Events View and Controller started;

---
+++ src/controllers/EventsController.js
@@ -0,0 +1,28 @@
+function getEvents(authKey) {
+    // Get events from database
+    // return {status: 200, message: "Events retrieved successfully", events: events};
+}
+
+function getEvent(id, authKey) {
+    // Get event from database
+    // return {status: 200, message: "Event retrieved successfully", event: event};
+}
+
+function createEvent(eventInfo, authKey) {
+    // Create event in database
+    // return {status: 200, message: "Event created successfully"};
+
+}
+
+function updateEvent(id, eventInfo, authKey) {
+    // Update event in database
+    // return {status: 200, message: "Event updated successfully"};
+
+}
+
+function deleteEvent(id, authKey) {
+    // Delete event from database
+    // return {status: 200, message: "Event deleted successfully"};
+
+}
+
--- src/views/Events.js
+++ src/views/Events.js
@@ -5,5 +5,82 @@ const BASE_PATH = "/events";
 function Events(app) {
 
 
+    /*** Gets Events Available for user
+     * @param {string} authKey
+     *
+     * return:
+     * @returns {string} message
+     * @returns {array} events
+     */
+    app.get(BASE_PATH, (req, res) => {
+        const {authKey} = req.body;
+
+        const response = getEvents(authKey);
+
+        res.status(response.status).send({message: response.message, events: response.events});
+    });
+
+    /*** Get Event by id
+     * @param {string} id - Event id
+     *
+     * return:
+     * @returns {string} message
+     * @returns {Object} event
+     */
+    app.get(`${BASE_PATH}/:id`, (req, res) => {
+        const {id} = req.params;
+
+        const response = getEvent(id);
+
+        res.status(response.status).send({message: response.message, event: response.event});
+    });
+
+    /*** Create Event
+     * @param {Object} eventInfo - Event information
+     * @param {string} authKey - User authenticator
+     *
+     * return:
+     * @returns {string} message
+     */
+    app.post(`${BASE_PATH}/create` , (req, res) => {
+        const {eventInfo, authKey} = req.body;
+
+        const response = createEvent(eventInfo, authKey);
+
+        res.status(response.status).send({message: response.message});
+    });
+
+    /*** Update Event by id
+     * @param {string} id - Event id
+     * * @param {Object} eventInfo - Event information
+     * * @param {string} authKey - User authenticator
+     * *
+     * * return:
+     * @returns {string} message
+     */
+    app.put(`${BASE_PATH}/:id`, (req, res) => {
+        const {id} = req.params;
+        const {eventInfo, authKey} = req.body;
+
+        const response = updateEvent(id, eventInfo, authKey);
+
+        res.status(response.status).send({message: response.message});
+    });
+
+    /*** Delete Event by id
+     * @param {string} id - Event id
+     * @param {string} authKey - User authenticator
+     *
+     * return:
+     * @returns {string} message
+     */
+    app.delete(`${BASE_PATH}/:id`, (req, res) => {
+        const {id} = req.params;
+        const {authKey} = req.body;
+
+        const response = deleteEvent(id, authKey);
+
+        res.status(response.status).send({message: response.message});
+    });
 
 }
